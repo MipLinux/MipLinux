cc183a37a51f3400206d87b370bbb6a79e21be4a
ideer040126 <703572351@qq.com>
2026-09-19 17:26:52 +0000

    更改 airootfs 的配置文件内容

--- profile/airootfs/etc/profile.d/tty-locale.sh
+++
@@ -1,9 +0,0 @@
-# 仅在物理 TTY 下把 locale 降级为英文，避免中文豆腐块
-if [ "$TERM" = "linux" ]; then
-    case "$(tty)" in
-        /dev/tty[1-6])
-            export LANG=en_US.UTF-8
-            export LANGUAGE=en_US
-            ;;
-    esac
-fi
